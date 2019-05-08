47d831e450ac9ef801529a84da3d39219495041a
tvibhu12 <tvibhu12@gmail.com>
2019-05-08 20:30:11 +0530

    setup project and display cards with details

---
+++ src/CardList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+const CardList=({robots})=>{
+    const cardComponent=robots.map((user)=>
+    {
+        return <Card key={user.id} id={user.id} name={user.name} email={user.email}/>
+    })
+    return(
+<div>
+{cardComponent};
+</div>
+    );
+}
+
+export default CardList;
